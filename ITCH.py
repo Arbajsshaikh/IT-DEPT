5534de170314ad54b0d4e0dfbf8b163c1e3f116f
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-01-10 11:19:10 +0530

    Update ITCH.py

--- ITCH.py
+++ ITCH.py
@@ -3,27 +3,7 @@ import streamlit as st
 import base64
 
 # Sample data
-df = pd.DataFrame({
-    'Bill No': ['WMPL/MFG/0553', 'WMPL/MFG/0555', 'WMPL/MFG/702', 'WMPL/MFG/795', 'WMPL/MFG/1002'],
-    'Doc. Date': ['22-10-2020', '22-10-2020', '28-11-2020', '11-12-2020', '29-01-2021'],
-    'Vou No': ['PW/10', 'PW/04', 'PW/296', 'PW/300', 'PW/730'],
-    'Bill Date': ['24-10-2020', '24-10-2020', '30-11-2020', '12-12-2020', '30-01-2021'],
-    'Party Name': ['WALMARK MEDITECH PVT.LTD.'] * 5,
-    'State': ['Maharashtra'] * 5,
-    'City Name': ['NAGPUR'] * 5,
-    'Product Name (Overwrite)': ['SUPREME ADULT DIAPER(XL) 10'] * 5,
-    'category': [None] * 5,
-    'HSN Code': [9619] * 5,
-    'Packing': [10] * 5,
-    'Qty': [10, 10, 10, 10, 10],
-    'Purchase Rate': [480.0, 240.0, 960.0, 480.0, 384.0],
-    'Purchase Amt': [96000.0, 48000.0, 192000.0, 96000.0, 76800.0],
-    'CGST Amount': [5760.0, 2880.0, 11520.0, 5760.0, 4608.0],
-    'SGST Amount': [5760.0, 2880.0, 11520.0, 5760.0, 4608.0],
-    'IGST Amount': [None] * 5,
-    'Total Amount': [107520.0, 53760.0, 215040.0, 107520.0, 86016.0],
-    'Category': ['SURGICAL'] * 5
-})
+df = pd.read_csv("CATEGORY_ADDED-Purchase Register 2020-21.csv")
 
 # Create dropdown for Vou No
 vou_no_options = df['Vou No'].unique()
@@ -60,12 +40,14 @@ if not filtered_data.empty:
         # Add an empty row
         custom_data = pd.concat([custom_data, pd.DataFrame(['', '']).T], ignore_index=True)
     
-        # Add column names in the 7th row
-        column_names = filtered_data.columns.tolist()
-        custom_data = pd.concat([custom_data, pd.DataFrame([column_names])], ignore_index=True)
+        # Get the list of columns excluding the ones used in the header
+        remaining_columns = [col for col in filtered_data.columns if col not in custom_headers]
     
-        # Add data values in the 8th row
-        custom_data = pd.concat([custom_data, pd.DataFrame([filtered_data.values.flatten()])], ignore_index=True)
+        # Add remaining columns in the 7th row
+        custom_data = pd.concat([custom_data, pd.DataFrame([remaining_columns])], ignore_index=True)
+    
+        # Add data values in the 8th row, excluding the columns used in the header
+        custom_data = pd.concat([custom_data, filtered_data[remaining_columns]], ignore_index=True)
     
         # Save to CSV with custom formatting
         csv_data = custom_data.to_csv(index=False, header=False)
@@ -73,6 +55,7 @@ if not filtered_data.empty:
         b64 = base64.b64encode(csv_data.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download Filtered Data as CSV</a>'
         st.markdown(href, unsafe_allow_html=True)
+
 
 
 # Trigger download automatically when a Vou No is selected
