7dcbf301f7e92e47b9573863d6e895cfed0db92c
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-01-10 11:04:30 +0530

    Update ITCH.py

--- ITCH.py
+++ ITCH.py
@@ -44,23 +44,28 @@ if not filtered_data.empty:
     st.table(filtered_data.drop(['Party Name', 'Bill No', 'Doc. Date', 'Vou No', 'Bill Date'], axis=1))
 
     # Function to download custom filtered data as CSV
-    def download_custom_filtered_data_csv(data, filename):
-        custom_header = pd.DataFrame({
-            'Custom Headers': [
-                'PARTY NAME=' + data['Party Name'].iloc[0],
-                'PARTY INVOICE NO=' + data['Bill No'].iloc[0],
-                'PARTY INVOICE DATE=' + data['Doc. Date'].iloc[0],
-                'GEN PUR NO=' + data['Vou No'].iloc[0],
-                'GEN PUR DATE=' + data['Bill Date'].iloc[0],
-            ]
-        })
+    def download_custom_filtered_data_csv(filtered_data, filename):
+        headers = {
+            'PARTY NAME': filtered_data['Party Name'].iloc[0],
+            'PARTY INVOICE NO': filtered_data['Bill No'].iloc[0],
+            'PARTY INVOICE DATE': filtered_data['Doc. Date'].iloc[0],
+            'GEN PUR NO': filtered_data['Vou No'].iloc[0],
+            'GEN PUR DATE': filtered_data['Bill Date'].iloc[0],
+        }
     
-        custom_data = pd.concat([custom_header, data.drop(['Party Name', 'Bill No', 'Doc. Date', 'Vou No', 'Bill Date'], axis=1)])
+        custom_data = pd.DataFrame(list(headers.items()), columns=['Custom Headers', 'Values'])
+        custom_data = custom_data.append(filtered_data.drop(['Party Name', 'Bill No', 'Doc. Date', 'Vou No', 'Bill Date'], axis=1))
     
-        csv_data = custom_data.to_csv(index=False)
+        # Save to CSV with custom formatting
+        csv_data = custom_data.to_csv(index=False, header=False)
+    
+        # Add eight empty lines before the data
+        csv_data = '\n' * 8 + csv_data
+    
         b64 = base64.b64encode(csv_data.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download Filtered Data as CSV</a>'
         st.markdown(href, unsafe_allow_html=True)
+
 
 # Trigger download automatically when a Vou No is selected
 if selected_vou_no:
